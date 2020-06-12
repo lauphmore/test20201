b438b5524899f2bdc384ab8a1d7e505bcad880fc
lauphmore <55043403+lauphmore@users.noreply.github.com>
2020-06-12 16:29:48 +0800

    Add files via upload

---
+++ Wrong_Printf_Scanf_false.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	scanf_s("%f", &a);
+	printf("%f", a);
+}
---
+++ Wrong_Printf_Scanf_true.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	scanf_s("%d", &a);
+	printf("%d", a);
+}
